e5636f1a54719a5a882af3539727579f573ba050
Ériko Fernando <eriko.bgoncalves@gmail.com>
2024-04-16 15:21:25 -0300

    Adicionando mais um caso de teste.

--- cypress/e2e/Auth/Login_Eriko.cy.js
+++ cypress/e2e/Auth/Login_Eriko.cy.js
@@ -17,6 +17,24 @@ describe('Atividade 3 - Autenticação de Usuário', function () {
         });
     });
 
+    it('Deve retornar um erro 401 caso a senha seja inserida errada', function () {
+        const name = faker.person.firstName()
+        const email = faker.internet.email({ firstName: name })
+        const password = faker.internet.password({ length: 6 })
+        cy.request({
+            method: 'POST',
+            url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/auth/login',
+            body: {
+                email: email,
+                password: password
+            },
+            failOnStatusCode: false
+        }).then(function (response) {
+            expect(response.status).to.equal(401)
+            expect(response.body.message).to.equal("Invalid username or password.")
+        });
+    });
+
     it('Deve retornar um erro 401 quando o usuário não existir', function () {
         const name = faker.person.firstName()
         const email = faker.internet.email({ firstName: name })
