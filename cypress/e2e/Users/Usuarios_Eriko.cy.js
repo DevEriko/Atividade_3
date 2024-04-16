50ef0b4e8f09647d3b98452cd816a266af96694a
Ériko Fernando <eriko.bgoncalves@gmail.com>
2024-04-16 14:24:46 -0300

    Adicionando mais casos de teste ao meus testes de criação de usuários

--- cypress/e2e/Users/Usuarios_Eriko.cy.js
+++ cypress/e2e/Users/Usuarios_Eriko.cy.js
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
-describe('Atividade 3 - Testes de API com Cypress', function () {
-    it('2°-> Autenticação de um usuário', function () {
+describe('Atividade 3 - Criação de usuário', function () {
+    it('Deve ser criado o usuário com nome, email, password corretos', function () {
         const name = faker.person.firstName()
         const email = faker.internet.email({ firstName: name })
         const password = faker.internet.password({ length: 6 })
@@ -14,14 +14,46 @@ describe('Atividade 3 - Testes de API com Cypress', function () {
             }
         }).then(function (response) {
             expect(response.status).to.equal(201)
-            cy.request({
-                method: 'POST',
-                url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/auth/login',
-                body: {
-                    email: email,
-                    password: password
-                },
-            });
+            expect(typeof response.body.id).to.equal('number')
+            expect(response.body).to.deep.include({ name, email })
+        });
+    });
+
+    it('Deve ser criado o usuário com o nome eriko', function () {
+        const name = "Eriko"
+        const email = faker.internet.email({ firstName: name })
+        const password = '123456'
+        cy.request({
+            method: 'POST',
+            url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users',
+            body: {
+                name: name,
+                email: email,
+                password: password
+            }
+        }).then(function (response) {
+            expect(response.status).to.equal(201)
+            expect(response.body.name).to.equal("Eriko")
+            expect(response.body.email).to.equal(email)
+        });
+    });
+
+    it('Deve retornar um erro ao tentar criar um usuário com email inválido', function () {
+        const name = "Eriko"
+        const email = "eriko.com.br"
+        const password = '123456'
+        cy.request({
+            method: 'POST',
+            url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users',
+            body: {
+                name: name,
+                email: email,
+                password: password
+            },
+            failOnStatusCode: false
+        }).then(function (response) {
+            expect(response.status).to.equal(400)
+            expect(response.body.message[0]).to.equal("email must be an email")
         });
     });
 });
