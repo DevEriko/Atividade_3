29f79e8bb88d6bfd084b225b7eb349daa2b8b4c9
Ériko Fernando <eriko.bgoncalves@gmail.com>
2024-04-16 14:24:22 -0300

    Adicionando mais casos de testes em meu login de usuário

--- cypress/e2e/Auth/Login_Eriko.cy.js
+++ cypress/e2e/Auth/Login_Eriko.cy.js
@@ -1,21 +1,37 @@
 import { faker } from '@faker-js/faker'
-describe('Atividade 3 - Testes de API com Cypress', function () {
-    it('1°-> Criação e consulta de um usuário', function () {
+describe('Atividade 3 - Autenticação de Usuário', function () {
+    it('Deve ser autenticado um usuário', function () {
+        const email = "usuarioeriko10@qa.com.br"
+        const password = "fernando"
+
+        cy.request({
+            method: 'POST',
+            url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/auth/login',
+            body: {
+                email: email,
+                password: password
+            },
+        }).then(function (response) {
+            expect(response.status).to.equal(200)
+            expect(typeof response.body.accessToken).to.equal('string')
+        });
+    });
+
+    it('Deve retornar um erro 401 quando o usuário não existir', function () {
         const name = faker.person.firstName()
         const email = faker.internet.email({ firstName: name })
         const password = faker.internet.password({ length: 6 })
         cy.request({
             method: 'POST',
-            url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/users',
+            url: 'https://raromdb-3c39614e42d4.herokuapp.com/api/auth/login',
             body: {
-                name: name,
                 email: email,
                 password: password
-            }
+            },
+            failOnStatusCode: false
         }).then(function (response) {
-            expect(response.status).to.equal(201)
-            expect(typeof response.body.id).to.equal('number')
-            expect(response.body).to.deep.include({ name, email })
+            expect(response.status).to.equal(401)
+            expect(response.body.message).to.equal("Invalid username or password.")
         });
     });
 });
